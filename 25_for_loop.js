989e239238b013d22a8212a85807da8b2424ea90
sandeep <sandeepmcodex.dev@gmail.com>
2025-07-31 11:06:17 +0530

    Add files via upload

---
+++ 25_for_loop.js
@@ -0,0 +1,44 @@
+// for loop
+
+// for (let index = 0; index <= 10; index++) {
+//   const element = index;
+//   console.log(element);
+// }
+// console.log(element);   //not defined
+
+for (let i = 0; i <= 10; i++) {
+  const element = i;
+  if (element == 5) {
+    //   console.log(" 5 is best number");
+  }
+  //   console.log(element);
+}
+
+for (let i = 1; i <= 10; i++) {
+  // console.log(`Outer loop value ${i}`);
+  for (let j = 1; j <= 10; j++) {
+    // console.log(`Inner loop value ${j}  and inner loop ${i}`);
+    // console.log(i+'*' +j +'=' + i*j);
+  }
+}
+
+
+let myArray =["flash " , "ironman" , "superman"]
+// console.log(myArray.length);
+for (let  index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  // console.log(element); 
+}
+
+
+// break and continue
+
+for (let index = 1; index <=20; index++) {
+  if(index ===5){
+    console.log(`5 is the best number `);
+    // break;
+    // continue;
+  }
+  console.log(`value of i is ${index}`);
+  
+}
