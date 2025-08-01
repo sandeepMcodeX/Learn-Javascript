8f76ce3e24f8d4bcf809c22a4c2ddd09de33033e
sandeep <sandeepmcodex.dev@gmail.com>
2025-08-01 19:53:35 +0530

    Add files via upload

---
+++ DOM/Episode_01/script.js
@@ -0,0 +1,9 @@
+// By id
+// const heading = document.getElementById("title");
+// console.log(heading);
+// By class
+const heading = document.getElementsByClassName('heading');
+console.log(heading);
+// By tag
+const paragraph = document.getElementsByTagName('p');
+console.log(paragraph);
