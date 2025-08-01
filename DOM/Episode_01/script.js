9e9b70978ec72d738fc7c593bc73cb4ba2072cc1
sandeep <sandeepmcodex.dev@gmail.com>
2025-08-01 19:53:58 +0530

    Delete DOM/Episode_01 directory

--- DOM/Episode_01/script.js
+++
@@ -1,9 +0,0 @@
-// By id
-// const heading = document.getElementById("title");
-// console.log(heading);
-// By class
-const heading = document.getElementsByClassName('heading');
-console.log(heading);
-// By tag
-const paragraph = document.getElementsByTagName('p');
-console.log(paragraph);
